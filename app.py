a54fce8bd1335ff36af9b186becfe0643cec9e98
Your Name <you@example.com>
2022-08-02 15:47:30 +0600

    homewroj

---
+++ app.py
@@ -0,0 +1,29 @@
+# from crypt import methods
+from distutils.debug import DEBUG
+from flask import Flask, render_template, request, redirect
+from peewee import *
+from models import Instagram
+
+app = Flask(__name__)
+
+@app.route('/')
+def lie():
+    all_instagram = Instagram.select()
+    return render_template("index.html", instagrams=all_instagram)
+
+@app.route('/create', methods=('GET', 'POST'))
+def create():
+    if request.method == 'POST':
+        user = request.form['user_name']
+        password = request.form['password']
+
+        Instagram.create(
+            user_name = user,
+            password = password
+        )
+        return redirect('/')
+    return render_template('create.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
---
+++ models.py
@@ -0,0 +1,31 @@
+# from email.policy import default
+# from click import password_option
+from peewee import *
+from datetime import datetime
+
+db = PostgresqlDatabase(
+    'skam',
+    host = 'localhost',
+    port = '5432',
+    user = 'adi',
+    password = 'qwe123'
+)
+
+db.connect()
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class Instagram(BaseModel):
+    user_name = CharField(max_length=255, null=False)
+    password = CharField(max_length=255, null=False)
+    date = DateField(default=datetime.now)
+
+    def __repr__(self):
+        return self.user_name
+
+# db.create_tables([Instagram])
+
+db.close
